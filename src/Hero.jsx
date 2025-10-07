9e0dac3b3e0c2f4bba887b665d4b38362237c170
adriantech-beep <Adhrianne29@gmail.com>
2025-10-07 10:49:13 +0800

    updated cv link

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         <IdentityBlock />
         <div className="mt-8 flex flex-col sm:flex-row gap-4 justify-center items-center z-10">
           <a
-            href="https://drive.google.com/file/d/1PSKORJH8DR64m9PfR6hTWvU1TYrpKrw4/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1fkFpTIBmmWry9bTgjuaIrCyBjKKJKDa7/view?usp=drive_link"
             download
             className="px-6 py-3 bg-cyan-500 hover:bg-cyan-400 text-black font-semibold rounded-md transition shadow-lg"
             aria-label="Download Adrian Fajardo's Resume"
